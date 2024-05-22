bf8e44362216995ca1f7055b26b67db159305da4
ayush.si <ayush.si@corp.zeta.tech>
2024-05-22 16:29:54 +0530

    test commit

---
+++ {{cookiecutter.sdk_name}}/tests/test_core.py
@@ -0,0 +1,16 @@
+"""Tests standard features using the built-in SDK tests library."""
+
+import unittest
+
+# standard test example. please delete it before writing you test cases 
+class TestStringMethods(unittest.TestCase):
+
+    def test_upper(self):
+        self.assertEqual('foo'.upper(), 'FOO')
+
+    def test_isupper(self):
+        self.assertTrue('FOO'.isupper())
+        self.assertFalse('Foo'.isupper())
+
+if __name__ == '__main__':
+    unittest.main()
